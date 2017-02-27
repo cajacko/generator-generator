38d1f20abb55e00844f120eb92b812ad803987a0
Charlie Jackson <charlie@charliejackson.com>
2017-02-27 10:15:27 +0000

    Split package check into a seperate npm package

--- generators/app/templates/generators/app/index.js
+++ generators/app/templates/generators/app/index.js
@@ -2,39 +2,12 @@ const Generator = require('yeoman-generator');
 const npmCheck = require('npm-check');
 const path = require('path');
 const npmName = require('../../package.json').name;
+const checkOutOfDatePackages = require('check-out-of-date-packages');
 
 module.exports = class extends Generator {
   initializing() {
     const cwd = path.join(__dirname, '../../');
-
-    return npmCheck({ cwd }).then((currentState) => {
-      const npmPackages = currentState.get('packages');
-      let needUpdating = false;
-
-      npmPackages.forEach((npmPackage) => {
-        if (npmPackage.latest !== npmPackage.installed) {
-          needUpdating = true;
-          this.log(`${npmName} Package out of date: ${npmPackage.moduleName} ${npmPackage.installed} -> ${npmPackage.latest}`);
-        }
-      });
-
-      if (needUpdating) {
-        return this.prompt([{
-          type: 'confirm',
-          name: 'continue',
-          message: 'This generator has packages which are out of date, do you want to continue?',
-          default: false
-        }]).then((answers) => {
-          if (answers.continue) {
-            return true;
-          }
-
-          throw new Error('Packages are out of date, please update and commit them before continuing.');
-        });
-      }
-
-      return true;
-    });
+    return checkOutOfDatePackages(cwd);
   }
 
   writing() {
